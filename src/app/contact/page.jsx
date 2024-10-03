b82642ea173d9960428efc913a8015b8f20b8fd2
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2024-10-03 16:23:54 +0100

    posts part

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,7 @@
 // "use client";
 import Image from "next/image";
 import styles from "./contact.module.css";
-// import dynamic from "next/dynamic";
-// import HydrationTest from "@/components/hydrationTest";
 
-// const HydrationTestNoSSR = dynamic(()=>import("@/components/hydrationTest"), {ssr: false})
 
 export const metadata = {
   title: "Contact Page",
@@ -12,9 +9,6 @@ export const metadata = {
 };
 
 const ContactPage = () => {
-  // const a = Math.random();
-
-  // console.log(a);
 
   return (
     <div className={styles.container}>
@@ -22,8 +16,7 @@ const ContactPage = () => {
         <Image src="/contact.png" alt="" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
-        {/* <HydrationTestNoSSR/> */}
-        {/* <div suppressHydrationWarning>{a}</div> */}
+      
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name and Surname" />
           <input type="text" placeholder="Email Address" />
