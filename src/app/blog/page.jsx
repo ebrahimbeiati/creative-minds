b61a4038fc5b44ee96dfb6ad3d38ad4691dbf1c2
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2024-10-06 23:44:10 +0100

    edit for postuser

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -12,15 +12,7 @@ const getData = async  () => {
   
 }
 
-/*************  ✨ Codeium Command ⭐  *************/
-/**
- * BlogPage component renders a list of blog posts.
- * The component fetches a list of posts from the API and renders each
- * post as a PostCard component.
- *
- * @return {JSX.Element} - A JSX element containing a list of PostCard components.
- */
-/******  86cdcb8d-f98a-4486-819b-1afb1d8c7897  *******/
+
 const BlogPage = async () => {
   const posts = await getData()
   return (
