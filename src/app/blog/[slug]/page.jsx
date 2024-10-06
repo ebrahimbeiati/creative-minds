1f77e4d399ca6785a92f4c13b3df1e262c102440
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2024-10-06 20:10:47 +0100

    edit

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -9,19 +9,27 @@ const SinglePage = () => {
       <div className={styles.textContainer}>
         <h1 className={styles.title}>Title</h1>
         <div className={styles.detail}>
-          <Image src="/avatar.png" alt="" width={50} height={50} className={styles.img} />
+          <Image
+            src="/avatar.png"
+            alt=""
+            width={50}
+            height={50}
+            className={styles.avatar}
+          />
         </div>
-        <div>
+        <div className={styles.detailText}>
           <span className={styles.detailTitle}>Author</span>
           <span className={styles.detailValue}>Ebrahim</span>
         </div>
-        <div>
+        <div className={styles.detailText}>
           <span className={styles.detailTitle}>Published</span>
           <span className={styles.detailValue}>01.02.2024</span>
         </div>
         <div className={styles.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus, deleniti maiores qui doloribus molestiae architecto sunt. Eveniet quisquam, sed quam laborum aut, totam repudiandae commodi, assumenda sunt ipsa atque?
-          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+          repellendus, deleniti maiores qui doloribus molestiae architecto sunt.
+          Eveniet quisquam, sed quam laborum aut, totam repudiandae commodi,
+          assumenda sunt ipsa atque?
         </div>
       </div>
     </div>
