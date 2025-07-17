712a09eb7c9a20f96865b74d6a4358e3b50e3903
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2025-07-17 21:21:30 +0100

    fixed the sign error

--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,21 +1,7 @@
+"use client";
 
-import { SignUp, useUser } from '@clerk/nextjs';
-import { useEffect } from 'react';
+import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  // const { isLoaded, user } = useUser();
-
-  // useEffect(() => {
-  //   if (isLoaded && user) {
-  //     fetch('/api/auth', {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify({ userId: user.id, email: user.primaryEmailAddress.emailAddress }),
-  //     });
-  //     }
-  //     console.log(isLoaded, user);
-
-  // }, [isLoaded, user]);
-
   return <SignUp />;
 }
